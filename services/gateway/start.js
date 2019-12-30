efb30d71667ef8a91de7b841ea3e35eb4680056b
gofrendi <gofrendiasgard@gmail.com>
2019-12-30 17:32:17 +0700

    2019-12-30T10:32:07.003Z Save changes before push to remote

--- services/gateway/start.js
+++ services/gateway/start.js
@@ -1,1 +1,23 @@
-console.log("gateway started")
+const http = require('http');
+const NATS = require('nats');
+const nats = NATS.connect();
+
+const port = process.env.port || 80;
+const subscribeEvent = process.env.sendMessageEvent || "bar";
+const publishEvent = process.env.getMessageEvent || "foo";
+
+//create a server object:
+console.log(`Serve HTTP on port ${port}`);
+http.createServer(function (req, res) {
+    nats.subscribe(subscribeEvent,
+        (receivedMessage) => {
+            console.log(`Receive request from url: ${req.url}`);
+            res.end(receivedMessage);
+        },
+        (error) => {
+            console.error(error);
+            res.end("Internal server error");
+        }
+    );
+    nats.publish(publishEvent, req.url);
+}).listen(port);
--- services/service/start.js
+++ services/service/start.js
@@ -1,1 +1,14 @@
-console.log("service started")
+const NATS = require('nats');
+const nats = NATS.connect();
+
+const subscribeEvent = process.env.getMessageEvent || "foo";
+const publishEvent = process.env.sendMessageEvent || "bar";
+const constantMessage = process.env.message || "et dah";
+
+console.log(`Listen to ${subscribeEvent} event`);
+nats.subscribe(subscribeEvent, (receivedMessage) => {
+    console.log(`Received a message: ${receivedMessage}`);
+    const publishedMessage = `${constantMessage} ${receivedMessage}`;
+    console.log(`Publish to ${publishEvent} event: ${publishedMessage}`);
+    nats.publish(publishEvent, publishedMessage);
+});
